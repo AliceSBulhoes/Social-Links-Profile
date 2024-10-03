69c32b891a37985586c51cf59476aa5213572df1
Alice <alicesb2006@gmail.com>
2024-10-03 17:17:31 -0300

    Criando variáveis de cor

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cinza: {
+          700: 'hsl(0, 0%, 20%)',
+          800: 'hsl(0, 0%, 12%)',
+          900: 'hsl(0, 0%, 8%)'
+        },
+        verde: {
+          100: 'hsl(75, 94%, 57%)'
+        },
+        branco: {
+          100: 'hsl(0, 0%, 100%)'
+        }
+      }
+    },
   },
   plugins: [],
 }
